99a6b173385789577e67545a968f64e4f44465a3
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-04-24 11:26:49 +0200

    created CsvFileReader class

--- build/index.js
+++ build/index.js
@@ -1,2 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+// readFileSync we can read any time of file
+// utf-8 -> of content we expect
+// parse into useable data structure
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+MatchResult.AwayWin;
+MatchResult.HomeWin;
+MatchResult.Draw;
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
 import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+
+// readFileSync we can read any time of file
+// utf-8 -> of content we expect
+
+// parse into useable data structure
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+MatchResult.AwayWin;
+MatchResult.HomeWin;
+MatchResult.Draw;
+
+let manUnitedWins = 0;
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
