45553de4f0c7901098e97999ab29e30a9eedbba8
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-04-28 12:09:49 +0200

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
 
-// Create an object that satisfies the "DataReader" interface
-// const cvsFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in something
-// satisfying the "DataReader" interface
-// const matchReader = new MatchReader(cvsFileReader);
 const matchReader = MatchReader.fromCsv('football.csv');
-matchReader.load();
-
-// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
